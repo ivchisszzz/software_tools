5375a3db2b00e6d69af046786d20fcdf89556237
Iva <ivadimitrova2107@gmail.com>
2022-05-22 23:04:36 +0300

    add first project with rest service

---
+++ software.tools/src/test/java/com/example/software/tools/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.software.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
